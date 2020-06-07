c6caf401a2e1ded9438f197339de84425e063fad
Pedro Marques <pmarques93@gmail.com>
2020-06-07 02:00:36 +0100

    Fix spelling errors

--- RogueLike/HighScore.cs
+++ RogueLike/HighScore.cs
@@ -24,7 +24,7 @@ namespace RogueLike
         /// Compares 2 HighScores
         /// </summary>
         /// <param name="otherScore">High score to compare</param>
-        /// <returns></returns>
+        /// <returns>Higher, lower or equal number</returns>
         public int CompareTo(HighScore otherScore)
         {
             if (otherScore == null) return - 1;

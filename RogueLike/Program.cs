3ee27b37328ce2a5db271431b7dd970a2d3b7bb2
Gabriel Santos <gabrielsantostroll@gmail.com>
2020-06-07 01:41:45 +0100

    Change command line arguments check
    It now checks the argument array length and tries to convert them to int.
    If the args array is to short or the convertion isn't possible it ends the app
    and prints an instructional error message.

--- RogueLike/Program.cs
+++ RogueLike/Program.cs
@@ -20,36 +20,42 @@ namespace RogueLike
             //Variable used to save the current game's seed
             int seed = (int)(currentTime.Ticks);
 
-            //Tries to start game and printing an error message if it fails
-            try
-            { 
-                //Checks if the player wrote columns first
-               if (args[0] == "-c" && args[2] == "-r")
+            // Checks if the argument attend the minimal length needed 
+            if (args.Length == 4)
+            {
+                int input1 = 0;
+                int input2 = 0;
+                // Tries to convert the input to integer
+                try
                 {
-                    //Converts the arguments to ints
-                    Game game = new Game(
-                        Convert.ToInt16(args[3]),
-                        Convert.ToInt16(args[1]),
-                        seed);
+                    input1 = Convert.ToInt16(args[1]);
+                    input2 = Convert.ToInt16(args[3]);
+                }
+
+                catch (FormatException)
+                {
+                    // Prints message and ends the application
+                    print.IntroErrorMessage();
+                    return;
                 }
 
                 //Checks if the player wrote columns first
+                if (args[0] == "-c" && args[2] == "-r")
+                {
+                    Game game = new Game(
+                        input2, input1, seed);
+                }
+                //Checks if the player wrote rows first
                 if (args[0] == "-r" && args[2] == "-c")
                 {
-                    //Converts the arguments to ints
                     Game game = new Game(
-                        Convert.ToInt16(args[1]),
-                        Convert.ToInt16(args[3]),
-                        seed);
+                        input1, input2, seed);
                 } 
             }
-            //Prints an Error message if the user inputs the wrong parameters 
-            catch (IndexOutOfRangeException)
-            {
+            // Prints the message if the input format doesn't attend
+            // the correct format
+            else
                 print.IntroErrorMessage();
-            }
-               
-
         }
     }
 }
